04af89a530f0f21c26fdeef09007ddbc6bb1a057
sgsuh <seukgyo.suh@gmail.com>
2025-11-02 03:15:18 +0000

    add ad classes in datasets

--- yolox/data/datasets/__init__.py
+++ yolox/data/datasets/__init__.py
@@ -4,6 +4,11 @@
 
 from .coco import COCODataset
 from .coco_classes import COCO_CLASSES
+from .coco_classes import AD_CLASSES
+from .coco_classes import AD_CLASSES_V2
+from .coco_classes import AD_CLASSES_V3
+from .coco_classes import AD_CLASSES_V4
+from .coco_classes import AD_CLASSES_V5
 from .datasets_wrapper import CacheDataset, ConcatDataset, Dataset, MixConcatDataset
 from .mosaicdetection import MosaicDetection
 from .voc import VOCDetection
